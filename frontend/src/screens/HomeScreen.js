a8e04c7ad615934497877331343cc7a31743ed01
Matt Ballard <mattjballard81@gmail.com>
2022-01-01 22:08:02 +0900

    create product component and home screen

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,19 @@
+import { Fragment } from 'react';
+import { Row, Col } from 'react-bootstrap';
+import products from '../products';
+import Product from '../components/Product';
+
+export default function HomeScreen() {
+  return (
+    <Fragment>
+      <h1>Featured Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </Fragment>
+  );
+}
